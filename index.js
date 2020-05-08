be17a56134738ee3f7225f801d5753403e6d39cd
root <root@DESKTOP-P0CFAEB.localdomain>
2020-05-08 23:22:32 +0800

    4

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-dataPath = '';
-dataFile = '108.csv';
-dataUrl = dataPath + dataFile;
-
-var datasets = [];
-var cities = [];
-var numbers = [];
-
-   
-console.log(datasets);
-
 var margin = {top: 40, right: 30, bottom: 40, left: 50},
-        width = 1500 - margin.left - margin.right,
-        height = 680 - margin.top - margin.bottom;
+        width = 1000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
 var greyColor = "#898989";
 var barColor = d3.interpolateInferno(0.4);
@@ -26,8 +15,8 @@ var svg = d3.select("#ff").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleBand()
-    .range([0, width])
-        .padding(0.4);    //長條形的寬度
+    .range([0, width/2])
+        .padding(0.2);    //長條形的寬度
 var y = d3.scaleLinear()
     .range([height, 0]);
 
@@ -58,13 +47,20 @@ var dataset = [{"city":"台北市", "value":61849},
                 {"city":"連江縣", "value":2999}];
 
 x.domain(dataset.map( d => { return d.city; }));
+
     // y.domain([0, d3.max(dataset,  d => { return d.value; })]);
-y.domain([1000, 140000]);
+y.domain([1000, 150000]);
 
 svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    .call(xAxis)
+    .selectAll("text") 
+    .style("text-anchor", "end") 
+    .attr("dx", "4em") 
+    .attr("dy", "-2.5em") 
+    .attr("transform", "rotate(90)");
+
 svg.append("g")
     .attr("class","y axis")
     .call(yAxis);
@@ -96,7 +92,7 @@ svg.selectAll(".label")
     .append("text")
     .attr("class", "label")
     .style("display",  d => { return d.value === null ? "none" : null; })
-    .attr("x", ( d => { return x(d.city) + (x.bandwidth() / 2) -20 ; }))
+    .attr("x", ( d => { return x(d.city) + (x.bandwidth()/2) -10 ; }))
         .style("fill",  d => {                                                  //字體highlight
             return d.value === d3.max(dataset,  d => { return d.value; }) 
             ? highlightColor : greyColor
